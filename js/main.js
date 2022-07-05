6228f864c5a8e5ed9e8ef5a4a9521dbd432b30ff
onyxV <averymc.dev@gmail.com>
2022-07-05 14:03:11 -0500

    Added variables from inputs, updated template literal string, removed unnecessary comments

--- js/main.js
+++ js/main.js
@@ -6,30 +6,11 @@ function makeNotes() {
   const telephone = document.querySelector('#telephone').value
   const location = document.querySelector('#location').value
   const tag = document.querySelector('#tag').value
-  // const lMidName = document.querySelector('#lastMiddle').value
-  // const lName = document.querySelector('#lastName').value
+  const problem = document.querySelector('#problem').value
+  const prevTickets = document.querySelector('#previous-tix').value
+  const troubleshoot = document.querySelector('#troubleshooting').value
+  const nextSteps = document.querySelector('#next-steps').value
+  const escalation = document.querySelector('#escalation').value
 
-  // document.querySelector('#placeToYell').innerText = fName + ' ' + fMidName + ' ' + ' ' + lMidName + ' ' + lName
-
-  //Add what you should be doing - conditionals go here
-
-  document.querySelector('#notes').innerText = `TMID: ${id}\n Name: ${name}\n Telephone: ${telephone}\n Location: ${location}\n Tag: ${tag}`// ${fMidName} ${lMidName} ${lName}`
+  document.querySelector('#notes').innerText = `TMID: ${id}\n Name: ${name}\n Telephone: ${telephone}\n Location: ${location}\n Tag: ${tag}\n ----------------------------------------------------------------------------------------------------\n Problem: ${problem}\n Previous Tickets: ${prevTickets}\n Troubleshooting: ${troubleshoot}\n Next Steps: ${nextSteps}\n Escalation: ${escalation}`// ${fMidName} ${lMidName} ${lName}`
 }
-
-// const synth = window.speechSynthesis;
-// document.querySelector('#yell').addEventListener('click', run)
-//
-// function run() {
-//   const fName = document.querySelector('#firstName').value
-//   const fMidName = document.querySelector('#firstMiddle').value
-//   const lMidName = document.querySelector('#lastMiddle').value
-//   const lName = document.querySelector('#lastName').value
-//
-//   const yellText =  `${fName} ${fMidName} ${lMidName} ${lName}`
-//
-//   document.querySelector('#placeToYell').innerText = yellText
-//
-//   let yellThis = new SpeechSynthesisUtterance(yellText);
-//
-//   synth.speak(yellThis);
-// }
